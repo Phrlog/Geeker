2e40781ba36ac35079fe341b6e0b7da6e24cc0e4
Phrlog <shiftiam@yandex.ru>
2016-09-21 02:02:36 +0300

    add admin controller

--- backend/controllers/AdminController.php
+++ backend/controllers/AdminController.php
@@ -1,7 +1,61 @@
 <?php
+namespace backend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use common\models\Geeks;
+use common\models\GeekForm;
+
 /**
- * Created by PhpStorm.
- * User: phrlog
- * Date: 20.09.16
- * Time: 22:57
+ * Site controller
  */
+class AdminController extends Controller
+{
+    public $layout = 'base';
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Create geek with current time
+     *
+     * @return string
+     */
+    public function actionCreateDateGeek()
+    {
+        $geek = new Geeks();
+
+        $time = date("H:i:s");
+
+        $geek->text = $time;
+        $result = $geek->save() ? "Твит с текстом $time успешно опубликован!" : "Неудача! Попробуйте снова.";
+
+        return $this->render('create-date-geek',[
+            'result' => $result
+        ]);
+    }
+
+    public function actionCreateGeek()
+    {
+        $result = null;
+        $model = new GeekForm();
+        $text = Yii::$app->request->post('GeekForm')['text'];
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            $geek = new Geeks();
+            $geek->text = $text;
+            $result = $geek->save() ? "Твит с текстом $text успешно опубликован!" : "Неудача! Попробуйте снова.";
+        }
+
+        return $this->render('create-geek', [
+            'model'  => $model,
+            'result' => $result
+        ]);
+    }
+}
