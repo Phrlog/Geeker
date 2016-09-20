bd7b080b73be9076cc53e81dcd30de62fa5ffe77
Phrlog <shiftiam@yandex.ru>
2016-09-21 02:02:15 +0300

    add backend layout and create-geek page

--- backend/views/admin/create-geek.php
+++ backend/views/admin/create-geek.php
@@ -1,7 +1,19 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: phrlog
- * Date: 20.09.16
- * Time: 23:29
- */
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+?>
+<?php $form = ActiveForm::begin(); ?>
+
+<?= $form->field($model, 'text')->label('Введите ваш твит:') ?>
+
+<div class="form-group">
+    <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
+</div>
+
+<?php ActiveForm::end(); ?>
+<?php if($result != null): ?>
+    <?= $result ?>
+<?php endif; ?>
+
