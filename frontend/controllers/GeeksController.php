bba48593a1c5bce87b6cf00c509e2e6638fe5625
Phrlog <shiftiam@yandex.ru>
2016-09-21 01:58:29 +0300

    add frontend geeks controller

--- frontend/controllers/GeeksController.php
+++ frontend/controllers/GeeksController.php
@@ -1,7 +1,40 @@
 <?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\web\Controller;
+use common\models\Geeks;
 /**
- * Created by PhpStorm.
- * User: phrlog
- * Date: 20.09.16
- * Time: 20:38
+ * Geeks controller
  */
+class GeeksController extends Controller
+{
+
+
+    public $layout = 'base';
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $geeks = new Geeks();
+        $geeks = $geeks->find()->all();
+
+        return $this->render('index',[
+            'geeks' => $geeks
+        ]);
+    }
+
+}
