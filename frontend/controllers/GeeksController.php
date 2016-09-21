e52a59dc7e7c741bd2672530755f96b35ba2ba4c
Phrlog <shiftiam@yandex.ru>
2016-09-21 17:03:13 +0300

    rename actionIndex method to actinGeeks, add redirect

--- frontend/controllers/GeeksController.php
+++ frontend/controllers/GeeksController.php
@@ -4,6 +4,8 @@ namespace frontend\controllers;
 use Yii;
 use yii\web\Controller;
 use common\models\Geeks;
+use yii\web\NotFoundHttpException;
+
 /**
  * Geeks controller
  */
@@ -29,12 +31,32 @@ class GeeksController extends Controller
      */
     public function actionIndex()
     {
+        return $this->actionGeeks();
+    }
+
+    public function actionGeeks()
+    {
         $geeks = new Geeks();
         $geeks = $geeks->find()->all();
 
-        return $this->render('index',[
+        return $this->render('geeks',[
             'geeks' => $geeks
         ]);
     }
+
+    public function actionView($id)
+    {
+        $geek = new Geeks();
+        $geek = $geek->findOne($id);
+
+        if ($geek === null) {
+            throw new NotFoundHttpException;
+        }
+
+        return $this->render('view', [
+            'geek' => $geek,
+        ]);
+    }
+
 
 }
