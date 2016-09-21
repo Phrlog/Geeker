9b60372bb94857758607fabc6549cfa868ae9618
Phrlog <shiftiam@yandex.ru>
2016-09-21 16:59:52 +0300

    add rules to UrlManager

--- common/config/main.php
+++ common/config/main.php
@@ -11,8 +11,13 @@ return [
             'showScriptName' => false,
             // Use pretty URLs
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => false,
             'rules' => [
-            ],
+                '/' => 'geeks/index',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+            ]
         ],
     ],
     'defaultRoute' => 'geeks'
