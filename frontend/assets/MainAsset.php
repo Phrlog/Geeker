737a60e5d461036b43c26fc47ec8b8b7daf7359f
Phrlog <shiftiam@yandex.ru>
2016-09-23 02:26:59 +0300

    fix asset

--- frontend/assets/MainAsset.php
+++ frontend/assets/MainAsset.php
@@ -7,24 +7,32 @@ use yii\web\AssetBundle;
 /**
  * Main frontend application asset bundle.
  */
-class GeekerAsset extends AssetBundle
+class MainAsset extends AssetBundle
 {
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
+
+        /* Bootstrap core CSS and Material Bootstrap */
         'css/bootstrap.min.css',
         'css/font-awesome.min.css',
         'css/bootstrap-material-design.css',
         'css/ripples.min.css',
         'css/material-scrolltop.css',
+
+        /*  Custom styles for this template */
         'css/material-blog.css',
+
+        /* Fonts */
         'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700',
         'https://fonts.googleapis.com/icon?family=Material+Icons'
     ];
     public $js = [
+
+        /* Loading animation  */
         'js/pace.min.js',
-        'https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js',
-        'https://oss.maxcdn.com/respond/1.4.2/respond.min.js',
+
+        /* libraries */
         'js/jquery-1.11.3.min.js',
         'js/ripples.min.js',
         'js/material.min.js',
