3e447685e3af13cb697651920f1baf8d3d9e7c7c
Phrlog <shiftiam@yandex.ru>
2016-09-22 16:35:38 +0300

    fix main backend asset

--- backend/assets/AdminAsset.php
+++ backend/assets/AdminAsset.php
@@ -23,11 +23,6 @@ class AdminAsset extends AssetBundle
         /* global styles */
         'css/compiled/theme_styles.css',
 
-        /* specific page */
-        'css/libs/daterangepicker.css',
-        'css/libs/jquery-jvectormap-1.2.2.css',
-        'css/libs/weather-icons.css',
-
         /* google font */
         '//fonts.googleapis.com/css?family=Open+Sans:400,600,700,300'
     ];
@@ -42,25 +37,9 @@ class AdminAsset extends AssetBundle
         'js/jquery.nanoscroller.min.js',
         'js/demo.js',
 
-        /* specific scripts */
-        'js/moment.min.js',
-        'js/jquery-jvectormap-1.2.2.min.js',
-        'js/jquery-jvectormap-world-merc-en.js',
-        'js/gdp-data.js',
-        'js/flot/jquery.flot.min.js',
-        'js/flot/jquery.flot.resize.min.js',
-        'js/flot/jquery.flot.time.min.js',
-        'js/flot/jquery.flot.threshold.js',
-        'js/flot/jquery.flot.axislabels.js',
-        'js/jquery.sparkline.min.js',
-        'js/skycons.js',
-
         /* theme script */
         'js/scripts.js',
         'js/pace.min.js',
-
-        /* init */
-        'init.js'
 
     ];
     public $depends = [
