1c81d7b1b19b5609611cf3d2a98b5312ba747fca
Phrlog <shiftiam@yandex.ru>
2016-09-23 22:12:48 +0300

    change title

--- backend/views/layouts/base.php
+++ backend/views/layouts/base.php
@@ -18,7 +18,7 @@ AdminAsset::register($this);
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
         <?= Html::csrfMetaTags() ?>
-        <title><?= Html::encode($this->title) ?><</title>
+        <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
     </head>
     <body>
