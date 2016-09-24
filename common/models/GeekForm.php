fcb0e603324ec03302aacf989c72b0217df46271
Phrlog <shiftiam@yandex.ru>
2016-09-24 22:52:02 +0300

    implement images upload

--- common/models/GeekForm.php
+++ common/models/GeekForm.php
@@ -1,17 +1,55 @@
 <?php
 namespace common\models;
 
+use Yii;
 use yii\base\Model;
+use yii\web\UploadedFile;
+use yii\imagine;
 
 class GeekForm extends Model
 {
     public $text;
 
+    /**
+     * @var UploadedFile
+     */
+    public $imageFile;
+
+
     public function rules()
     {
         return [
-            [['text', 'text'], 'required']
+            [['text', 'text'], 'required'],
+            [['imageFile'], 'file', 'skipOnEmpty' => false, 'extensions' => 'png, jpg']
         ];
     }
 
+    public function upload()
+    {
+        if ($this->validate()) {
+            $path = Yii::getAlias('uploads/');
+            $this->createDir($path);
+
+            $file_name = $this->imageFile->baseName . '.' . $this->imageFile->extension;
+
+            $this->imageFile->saveAs('uploads/' . $file_name);
+
+            $path = Yii::getAlias('uploads/thumbnail/');
+            $this->createDir($path);
+
+            imagine\Image::thumbnail( 'uploads/' . $file_name, 240, 320)
+                ->save('uploads/thumbnail/' . $file_name, ['quality' => 50]);
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function createDir($path)
+    {
+        if (!file_exists($path)) {
+            mkdir($path, 0775, true);
+        }
+    }
 }
