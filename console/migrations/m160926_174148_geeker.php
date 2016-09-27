a6d8ecb94746984157c6406760c84c09e49af6e6
Phrlog <shiftiam@yandex.ru>
2016-09-28 02:48:16 +0300

    add geeks migration

--- console/migrations/m160926_174148_geeker.php
+++ console/migrations/m160926_174148_geeker.php
@@ -4,26 +4,32 @@ use yii\db\Migration;
 
 class m160926_174148_geeker extends Migration
 {
-    public function up()
+    public function SafeUp()
     {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
 
+        $this->createTable('{{%geeks}}', [
+            'id' => $this->primaryKey(),
+            'user_id' => $this->integer()->notNull(),
+            'text' => $this->string()->notNull(),
+            'image' => $this->string(255),
+            'thumbnail' => $this->string(255),
+            'created_at' => $this->dateTime()->notNull(),
+            'updated_at' => $this->dateTime()->notNull(),
+        ], $tableOptions);
+
+        $this->createIndex("geek_user", "{{%geeks}}", "user_id");
+        $this->addForeignKey("FK_geek_user", "{{%geeks}}", "user_id", "{{%user}}", "id");
     }
 
-    public function down()
+    public function SafeDown()
     {
-        echo "m160926_174148_geeker cannot be reverted.\n";
-
-        return false;
+        $this->dropForeignKey("FK_geek_user", "{{%geeks}}");
+        $this->dropTable('{{%geeks}}');
     }
 
-    /*
-    // Use safeUp/safeDown to run migration code within a transaction
-    public function safeUp()
-    {
-    }
-
-    public function safeDown()
-    {
-    }
-    */
 }
