e064765f3acb8d0d048bec9c91be6bc212cc0590
Phrlog <shiftiam@yandex.ru>
2016-09-28 02:49:13 +0300

    improve geeks model, add getAuthor() method

--- common/models/Geeks.php
+++ common/models/Geeks.php
@@ -3,14 +3,22 @@
 namespace common\models;
 
 use Yii;
+use yii\behaviors\TimestampBehavior;
+use yii\db\Expression;
+use yii\db\ActiveRecord;
 
 /**
  * This is the model class for table "{{%geeks}}".
  *
  * @property integer $id
+ * @property integer $user_id
  * @property string $text
  * @property string $image
  * @property string $thumbnail
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property User $user
  */
 class Geeks extends \yii\db\ActiveRecord
 {
@@ -23,15 +31,35 @@ class Geeks extends \yii\db\ActiveRecord
     }
 
     /**
+     * Add date before publication
+     *
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => '\yii\behaviors\TimestampBehavior',
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['created_at', 'updated_at'],
+                    ActiveRecord::EVENT_BEFORE_UPDATE => ['updated_at'],
+                ],
+                'value' => new Expression('NOW()'),
+            ],
+        ];
+    }
+
+    /**
      * @inheritdoc
      */
     public function rules()
     {
         return [
-            [['text'], 'required'],
-            [['text'], 'string', 'max' => 20000],
-            [['image'], 'string', 'max' => 255],
-            [['thumbnail'], 'string', 'max' => 255]
+            [['user_id', 'text'], 'required'],
+            [['user_id'], 'integer'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['text', 'image', 'thumbnail'], 'string', 'max' => 255],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
         ];
     }
 
@@ -42,9 +70,28 @@ class Geeks extends \yii\db\ActiveRecord
     {
         return [
             'id' => 'ID',
+            'user_id' => 'User ID',
             'text' => 'Text',
             'image' => 'Image',
-            'thumbnail' => 'Thumbnail'
+            'thumbnail' => 'Thumbnail',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At'
         ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * @return \common\models\User;
+     */
+    public function getAuthor()
+    {
+        return User::find()->where(['id' => $this->user_id])->one();
     }
 }
