70732891ea76cf84bb1584d63b5186baa7ad83c4
Phrlog <shiftiam@yandex.ru>
2016-09-28 17:26:27 +0300

    improve login page

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -7,33 +7,22 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Login';
-$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
+<section class="blog-post">
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="blog-post-content">
+                <h2 class="blog-post-title">Inline Form</h2>
+                <?php $form = ActiveForm::begin(['class' => 'form-inline']); ?>
+                <label class="sr-only" for="exampleInputEmail2">Email address</label>
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
+                <label class="sr-only" for="exampleInputPassword2">Password</label>
                 <?= $form->field($model, 'password')->passwordInput() ?>
-
                 <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
-                </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
-</div>
+</section><!-- /.blog-post -->
+
