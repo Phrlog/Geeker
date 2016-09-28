39794cb04b7d6529af688464a14c02e32cb3b0f6
Phrlog <shiftiam@yandex.ru>
2016-09-29 00:20:37 +0300

    change page, add textarea

--- frontend/views/geeks/create.php
+++ frontend/views/geeks/create.php
@@ -6,28 +6,30 @@ use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\bootstrap\Alert;
 ?>
-<?php $form = ActiveForm::begin(); ?>
+<div class="panel panel-default">
+    <div class="panel-body">
+        <?php $form = ActiveForm::begin(); ?>
+        <?= $form->field($model, 'text')->textarea()->label('Введите ваш твит:') ?>
+        <?= $form->field($model, 'imageFile', ['template' => '<i class="fa fa-file-image-o" aria-hidden="true"></i>{input} {label}'])->fileInput(['class' => 'file']) ?>
+        <div class="form-group">
+            <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
+        </div>
 
-<?= $form->field($model, 'text')->label('Введите ваш твит:') ?>
+        <?php ActiveForm::end(); ?>
 
-<?= $form->field($model, 'imageFile')->fileInput() ?>
+        <?php if (Yii::$app->session->hasFlash('success')): ?>
+            <?= Alert::widget([
+                'options' => ['class' => 'alert alert-success'],
+                'body' => Yii::$app->session->getFlash('success')
+            ]);?>
 
-<div class="form-group">
-    <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
+        <?php elseif (Yii::$app->session->hasFlash('error')): ?>
+            <?= Alert::widget([
+                'options' => ['class' => 'alert alert-danger'],
+                'body' => Yii::$app->session->getFlash('error')
+            ]);?>
+        <?php endif; ?>
+    </div>
 </div>
 
-<?php ActiveForm::end(); ?>
-
-<?php if (Yii::$app->session->hasFlash('success')): ?>
-    <?= Alert::widget([
-        'options' => ['class' => 'alert alert-success'],
-        'body' => Yii::$app->session->getFlash('success')
-    ]);?>
-
-<?php elseif (Yii::$app->session->hasFlash('error')): ?>
-    <?= Alert::widget([
-        'options' => ['class' => 'alert alert-danger'],
-        'body' => Yii::$app->session->getFlash('error')
-    ]);?>
-<?php endif; ?>
 
