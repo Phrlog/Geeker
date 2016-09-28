8cee920e64ac590d3bbc20303294dc0f682693ac
Phrlog <shiftiam@yandex.ru>
2016-09-28 17:27:11 +0300

    add rules to url manager

--- common/config/main.php
+++ common/config/main.php
@@ -14,6 +14,8 @@ return [
             'enableStrictParsing' => false,
             'rules' => [
                 '/' => 'geeks/index',
+                'logout' => 'site/logout',
+                'login' => 'site/login',
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
