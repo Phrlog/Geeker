fcce7062edf6f06319601088635afffca0e6a6d3
Phrlog <shiftiam@yandex.ru>
2016-09-29 00:23:13 +0300

    minor fix

--- backend/views/geeks/show-geeks.php
+++ backend/views/geeks/show-geeks.php
@@ -23,11 +23,9 @@ use yii\helpers\Url;
         <div class="main-box-body clearfix">
             <a href="<?= Url::to(['geeks/edit', 'id' => $geek->id]); ?>" >
                 <i class="fa fa-edit"></i>
-                Изменить
             </a>
             <a href="<?= Url::to(['geeks/delete', 'id' => $geek->id]); ?>">
                 <i class="fa fa-trash-o"></i>
-                Удалить
             </a>
         </div>
     </div>
