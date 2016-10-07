8584ba819686ffeb3d7e672be2656d35e03c2d3f
Phrlog <shiftiam@yandex.ru>
2016-10-07 21:30:33 +0300

    fix migration

--- console/migrations/m161007_140207_parent_geek.php
+++ console/migrations/m161007_140207_parent_geek.php
@@ -7,10 +7,12 @@ class m161007_140207_parent_geek extends Migration
     public function SafeUp()
     {
         $this->addColumn("{{%geeks}}", 'parent_id', $this->integer()->defaultValue(NULL) . ' AFTER user_id');
+        $this->addForeignKey("FK_parent_geek", "{{%geeks}}", "parent_id", "{{%geeks}}", "id");
     }
 
     public function SafeDown()
     {
+        $this->dropForeignKey("FK_parent_geek", "{{%geeks}}");
         $this->dropColumn("{{%geeks}}", 'parent_id');
     }
 
