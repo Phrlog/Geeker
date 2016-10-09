7c12f772b255eee7c803921a1c20f0a9a48ec4f6
Phrlog <shiftiam@yandex.ru>
2016-10-09 22:37:14 +0300

    asset for modal form

---
+++ frontend/assets/AnswerAsset.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace frontend\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main frontend application asset bundle.
+ */
+class AnswerAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+
+    ];
+    public $js = [
+        'js/modal.js'
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset'
+    ];
+}
