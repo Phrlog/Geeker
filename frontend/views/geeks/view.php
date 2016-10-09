32b8cffdef5a65e390fe2fc11c445cf1e7a066de
Phrlog <shiftiam@yandex.ru>
2016-10-09 22:41:03 +0300

    add answer button

--- frontend/views/geeks/view.php
+++ frontend/views/geeks/view.php
@@ -5,8 +5,11 @@ use yii\helpers\Html;
 use yii\helpers\Url;
 use common\models\Likes;
 use frontend\assets\LikesAsset;
+use frontend\assets\AnswerAsset;
 
 LikesAsset::register($this);
+AnswerAsset::register($this);
+
 ?>
 
 <div class="col-sm-12">
@@ -18,7 +21,9 @@ LikesAsset::register($this);
             <div class="panel-body">
                 <div class="blog-post-meta">
                     <a href="<?= Url::to(['user/profile', 'id' => $geek->user_id]) ?>">
-                        <span class="label label-light label-primary"><?= Html::encode($geek->getUser()->select(['username'])->one()->username) ?></span></a>
+                        <span class="label label-light label-primary">
+                            <?= Html::encode($geek->getUser()->select(['username'])->one()->username) ?>
+                        </span></a>
                     <p class="blog-post-date pull-right"><?= $geek->updated_at ?></p>
                 </div>
                 <div class="blog-post-content">
@@ -27,8 +32,13 @@ LikesAsset::register($this);
                     </a>
                     <div class="like-panel" id="<?= $geek->id ?>">
                         <span><?= $geek->count ?></span>
-                        <button type="button" data-url="<?= Url::to(['geeks/like'], true) ?>" data-id="<?= $geek->id ?>" class="btn btn-primary like">
-                            <i class="fa fa-heart <?= Likes::isRelationExist(\Yii::$app->user->id, $geek->id) ? "like" : '';?>"></i>
+                        <button type="button" data-url="<?= Url::to(['geeks/like'], true) ?>"
+                                data-id="<?= $geek->id ?>" class="btn btn-primary like">
+                            <i class="fa fa-heart <?= Likes::isRelationExist(\Yii::$app->user->id, $geek->id) ? "like" : ''; ?>"></i>
+                        </button>
+
+                        <button type="button" data-url="<?= Url::to(['geeks/answer'], true) ?>" data-id="<?= $geek->id ?>" class="btn btn-primary answer">
+                            <i class="fa fa-reply""></i>
                         </button>
                     </div>
                     <a class="blog-post-share pull-right" href="#">
@@ -40,36 +50,42 @@ LikesAsset::register($this);
     </section>
 </div>
 <?php if ($answers != null): ?>
-<?php foreach ($answers as $answer): ?>
-<div class="col-sm-8">
-    <section class="blog-post">
-        <div class="panel panel-default">
-            <?php if ($answer->thumbnail): ?>
-                <?= Html::img(Yii::$app->UrlManager->createUrl($answer->image), ['class' => "img-responsive"]) ?>
-            <?php endif; ?>
-            <div class="panel-body">
-                <div class="blog-post-meta">
-                    <a href="<?= Url::to(['user/profile', 'id' => $answer->user_id]) ?>">
-                        <span class="label label-light label-primary"><?= Html::encode($answer->getUser()->select(['username'])->one()->username) ?></span></a>
-                    <p class="blog-post-date pull-right"><?= $answer->updated_at ?></p>
-                </div>
-                <div class="blog-post-content">
-                    <a href="<?= Url::to(['geeks/view', 'id' => $answer->id]); ?>">
-                        <h2 class="blog-post-title"><?= Html::encode($answer->text) ?></h2>
-                    </a>
-                    <div class="like-panel" id="<?= $answer->id ?>">
-                        <span><?= $answer->count ?></span>
-                        <button type="button" data-url="<?= Url::to(['geeks/like'], true) ?>" data-id="<?= $answer->id ?>" class="btn btn-primary like">
-                            <i class="fa fa-heart <?= Likes::isRelationExist(\Yii::$app->user->id, $answer->id) ? "like" : '';?>"></i>
-                        </button>
+    <?php foreach ($answers as $answer): ?>
+        <div class="col-sm-8">
+            <section class="blog-post">
+                <div class="panel panel-default">
+                    <?php if ($answer->thumbnail): ?>
+                        <?= Html::img(Yii::$app->UrlManager->createUrl($answer->image), ['class' => "img-responsive"]) ?>
+                    <?php endif; ?>
+                    <div class="panel-body">
+                        <div class="blog-post-meta">
+                            <a href="<?= Url::to(['user/profile', 'id' => $answer->user_id]) ?>">
+                                <span class="label label-light label-primary">
+                                    <?= Html::encode($answer->getUser()->select(['username'])->one()->username) ?>
+                                </span></a>
+                            <p class="blog-post-date pull-right"><?= $answer->updated_at ?></p>
+                        </div>
+                        <div class="blog-post-content">
+                            <a href="<?= Url::to(['geeks/view', 'id' => $answer->id]); ?>">
+                                <h2 class="blog-post-title"><?= Html::encode($answer->text) ?></h2>
+                            </a>
+                            <div class="like-panel" id="<?= $answer->id ?>">
+                                <span><?= $answer->count ?></span>
+                                <button type="button" data-url="<?= Url::to(['geeks/like'], true) ?>" data-id="<?= $answer->id ?>" class="btn btn-primary like">
+                                    <i class="fa fa-heart <?= Likes::isRelationExist(\Yii::$app->user->id, $answer->id) ? "like" : ''; ?>"></i>
+                                </button>
+
+                                <button type="button" data-url="<?= Url::to(['geeks/answer'], true) ?>" data-id="<?= $answer->id ?>" class="btn btn-primary answer">
+                                    <i class="fa fa-reply""></i>
+                                </button>
+                            </div>
+                            <a class="blog-post-share pull-right" href="#">
+                                <i class="material-icons">&#xE80D;</i>
+                            </a>
+                        </div>
                     </div>
-                    <a class="blog-post-share pull-right" href="#">
-                        <i class="material-icons">&#xE80D;</i>
-                    </a>
                 </div>
-            </div>
+            </section>
         </div>
-    </section>
-</div>
-<?php endforeach; ?>
+    <?php endforeach; ?>
 <?php endif; ?>
