88027a08d937396d16e74f5f85ae65d0d6c6a540
Phrlog <shiftiam@yandex.ru>
2016-10-10 18:47:47 +0300

    improve markup

--- frontend/views/user/search.php
+++ frontend/views/user/search.php
@@ -3,18 +3,22 @@
 use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 use yii\jui\AutoComplete;
-?>
 
-<?php $form = ActiveForm::begin(); ?>
-<?= $form->field($model, 'username')->widget(
-    AutoComplete::className(), [
-    'clientOptions' => [
-        'source' => $users,
-    ],
-    'options'=>[
-        'class'=>'form-control'
-    ]
-]);
 ?>
-<?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
-<?php ActiveForm::end(); ?>
+<div class="panel panel-default">
+    <div class="panel-body">
+        <?php $form = ActiveForm::begin(); ?>
+        <?= $form->field($model, 'username')->widget(
+            AutoComplete::className(), [
+            'clientOptions' => [
+                'source' => $users,
+            ],
+            'options' => [
+                'class' => 'form-control'
+            ]
+        ]);
+        ?>
+        <?= Html::submitButton('Отправить', ['class' => 'btn btn-primary']) ?>
+        <?php ActiveForm::end(); ?>
+    </div>
+</div>
