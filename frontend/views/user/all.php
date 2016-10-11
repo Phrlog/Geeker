a84852cd0ba51260554e975495f86cd8616313f8
Phrlog <shiftiam@yandex.ru>
2016-10-11 21:47:13 +0300

    refactor templates

--- frontend/views/user/all.php
+++ frontend/views/user/all.php
@@ -15,28 +15,7 @@ $this->title = Html::encode($title);
         <div class="col-sm-12">
             <h2><?= $this->title ?></h2>
             <?php foreach ($users as $user): ?>
-                <section class="blog-post">
-                    <div class="panel panel-default">
-                        <div class="panel-body">
-                            <div class="blog-post-content">
-                                <a href="<?= Url::to(['user/profile', 'id' => $user->id]) ?>">
-                                    <h2 class="blog-post-title"><?= Html::encode($user->username) ?></h2></a>
-                                <p>Подписаны: <a href="<?= Url::to(['user/subscribers', 'id' =>  $user->id]) ?>"><b><?= $user->subscribers ?></b></p></a>
-                                <p>Подписан: <a href="<?= Url::to(['user/subscriptions', 'id' =>  $user->id]) ?>"><b><?= $user->subscriptions ?></b></p></a>
-                                <?php if (Subscription::isRelationExist(Yii::$app->user->id, $user->id) && (Yii::$app->user->id != $user->id)): ?>
-                                    <a href="<?= Url::to(['user/unsubscribe', 'id' => $user->id]); ?>">
-                                        <button type="button" class="btn btn-success btn-lg subscribe_button">Подписаны
-                                        </button>
-                                    </a>
-                                <?php elseif (Yii::$app->user->id != $user->id): ?>
-                                    <a href="<?= Url::to(['user/subscribe', 'id' => $user->id]); ?>">
-                                        <button type="button" class="btn btn-info btn-lg">Подписаться</button>
-                                    </a>
-                                <?php endif; ?>
-                            </div>
-                        </div>
-                    </div>
-                </section>
+                <?php include '_user.php' ?>
             <?php endforeach; ?>
         </div>
     </div>
--- frontend/views/user/friends.php
+++ frontend/views/user/friends.php
@@ -15,57 +15,13 @@ use common\models\Subscription;
             <div class="col-sm-6">
                 <h1>Мои подписки</h1>
                 <?php foreach ($subscriptions as $user): ?>
-                    <section class="blog-post">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <div class="blog-post-content">
-                                    <a href="<?= Url::to(['user/profile', 'id' => $user->id]) ?>"><h2
-                                            class="blog-post-title"><?= Html::encode($user->username) ?></h2></a>
-                                    <p>Подписаны: <a href="<?= Url::to(['user/subscribers', 'id' =>  $user->id]) ?>"><b><?= $user->subscribers ?></b></p></a>
-                                    <p>Подписан: <a href="<?= Url::to(['user/subscriptions', 'id' =>  $user->id]) ?>"><b><?= $user->subscriptions ?></b></p></a>
-                                    <?php if (Subscription::isRelationExist(Yii::$app->user->id, $user->id) && (Yii::$app->user->id != $user->id)): ?>
-                                        <a href="<?= Url::to(['user/unsubscribe', 'id' => $user->id]); ?>">
-                                            <button type="button" class="btn btn-success btn-lg subscribe_button">
-                                                Подписан
-                                            </button>
-                                        </a>
-                                    <?php elseif (Yii::$app->user->id != $user->id): ?>
-                                        <a href="<?= Url::to(['user/subscribe', 'id' => $user->id]); ?>">
-                                            <button type="button" class="btn btn-info btn-lg">Подписаться</button>
-                                        </a>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+                    <?php include '_user.php' ?>
                 <?php endforeach; ?>
             </div>
             <div class="col-sm-6">
                 <h1>Мои подписчики</h1>
                 <?php foreach ($subscribers as $user): ?>
-                    <section class="blog-post">
-                        <div class="panel panel-default">
-                            <div class="panel-body">
-                                <div class="blog-post-content">
-                                    <a href="<?= Url::to(['user/profile', 'id' => $user->id]) ?>"><h2
-                                            class="blog-post-title"><?= Html::encode($user->username) ?></h2></a>
-                                    <p>Подписаны: <a href="<?= Url::to(['user/subscribers', 'id' =>  $user->id]) ?>"><b><?= $user->subscribers ?></b></p></a>
-                                    <p>Подписан: <a href="<?= Url::to(['user/subscriptions', 'id' =>  $user->id]) ?>"><b><?= $user->subscriptions ?></b></p></a>
-                                    <?php if (Subscription::isRelationExist(Yii::$app->user->id, $user->id) && (Yii::$app->user->id != $user->id)): ?>
-                                        <a href="<?= Url::to(['user/unsubscribe', 'id' => $user->id]); ?>">
-                                            <button type="button" class="btn btn-success btn-lg subscribe_button">
-                                                Подписаны
-                                            </button>
-                                        </a>
-                                    <?php elseif (Yii::$app->user->id != $user->id): ?>
-                                        <a href="<?= Url::to(['user/subscribe', 'id' => $user->id]); ?>">
-                                            <button type="button" class="btn btn-info btn-lg">Подписаться</button>
-                                        </a>
-                                    <?php endif; ?>
-                                </div>
-                            </div>
-                        </div>
-                    </section>
+                    <?php include '_user.php' ?>
                 <?php endforeach; ?>
             </div>
         </div>
