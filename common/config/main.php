91d3f7e0f45bf82e6d27d949521daeaab16ebf66
Phrlog <shiftiam@yandex.ru>
2016-10-21 02:40:53 +0300

    minor fix

--- common/config/main.php
+++ common/config/main.php
@@ -16,6 +16,7 @@ return [
                 '/' => 'geeks/index',
                 'logout' => 'site/logout',
                 'login' => 'site/login',
+                'about' => 'site/about',
                 '<controller:\w+>/<id:\d+>' => '<controller>/view',
                 '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
                 '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
