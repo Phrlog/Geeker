6c050d509856c26a2b7e8c4806956427a73d9714
Phrlog <shiftiam@yandex.ru>
2016-10-25 15:27:52 +0300

    replace logic

--- common/models/Geeks.php
+++ common/models/Geeks.php
@@ -160,24 +160,4 @@ class Geeks extends \yii\db\ActiveRecord
         return $geek;
     }
 
-    /**
-     * @param $user_id
-     * @param int $sort
-     * @return array|\yii\db\ActiveRecord[]
-     */
-    public static function getUserFeed($user_id, $sort = SORT_DESC) {
-        $geeks = Geeks::find()->select(['geeks.*', 'user.username', 'COUNT(likes.geek_id) as count'])
-            ->join('INNER JOIN', User::tableName(), 'user.id = geeks.user_id')
-            ->join('INNER JOIN', Subscription::tableName(), 'subscription.subscribe_id = user.id')
-            ->join('LEFT JOIN', Likes::tableName(), 'likes.geek_id = geeks.id')
-            ->where(['subscription.user_id' => $user_id])
-            ->orWhere(['geeks.user_id' => $user_id])
-            ->groupBy(['geeks.id'])
-            ->orderBy(['geeks.created_at' => $sort])
-            ->all();
-
-        return $geeks !== null ? $geeks: [];
-
-    }
-
 }
--- common/models/User.php
+++ common/models/User.php
@@ -248,62 +248,6 @@ class User extends ActiveRecord implements IdentityInterface
     }
 
     /**
-     * @param $id
-     * @param array $param
-     * @return array|bool
-     */
-    public static function getUsersId($id, array $param)
-    {
-        $users_id = false;
-
-        $query = new Query;
-        $query->select($param['select'])
-            ->from('subscription')
-            ->where([$param['where'] => $id]);
-        $users = $query->all();
-        for ($i = 0; $i < count($users); $i++){
-            $users_id[] = $users[$i][$param['select']];
-        }
-
-        return $users_id;
-    }
-
-    /**
-     * If $id === true, return all users.
-     * If $id === false, return empty array
-     * Else, return users with $id
-     *
-     * @param array|bool|int $id
-     * @return array|\yii\db\ActiveRecord[]
-     */
-    public static function getUsersById($id = true)
-    {
-        $users =  User::find()
-            ->select(['user.*', 'count(distinct s.subscribe_id) as subscriptions', ' count(distinct p.user_id) as subscribers'])
-            ->join('LEFT JOIN', [Subscription::tableName(). ' s'], 's.user_id = user.id')
-            ->join('LEFT JOIN', [Subscription::tableName(). ' p'], 'p.subscribe_id = user.id');
-
-        if ($id === true) {
-            return $users->groupBy(['user.id'])->all();
-        } elseif ($id === false) {
-            return [];
-        }
-
-        return $users->where(['user.id' => $id])->groupBy(['user.id'])->all();
-    }
-
-    /**
-     * Get subscriptions id of user by his id
-     *
-     * @param $user_id
-     * @return array
-     */
-    public static function getSubscribersId($user_id) {
-        $param = ['select' => 'subscribe_id', 'where' => 'user_id'];
-        return $user_id ? User::getUsersId(Yii::$app->user->id, $param) : [];
-    }
-
-    /**
      * @param $user_id
      * @return null|static
      * @throws NotFoundHttpException
